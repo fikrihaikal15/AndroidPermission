cd2d17caf860d1d18982d660b47eaa9bb2ad888f
Fikri Haikal <fikrihaikal571@gmail.com>
2023-04-17 23:08:36 +0700

    Fikri Haikal- Latihan1(NormalPermission),Latihan2(TurnOffOnWifi),Latihan3(DangerousPermission),Latihan4(CameraPermission)

---
+++ app/src/main/java/com/example/androidpermission/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.androidpermission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.androidpermission.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
